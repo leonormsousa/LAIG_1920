eb4588cd399697a9aec4516ae28e61304424db83
leonormsousa <20341608+leonormsousa@users.noreply.github.com>
2019-09-30 15:00:20 +0100

    Corrected sphere

--- Proj/MySphere.js
+++ Proj/MySphere.js
@@ -20,8 +20,14 @@ class MySphere extends CGFobject
 		var angle = (2* Math.PI) / this.slices;
 		var delta = (Math.PI/2) / this.stacks;
 
-        this.vertices.push(0, 0, -this.radius)
-        this.normals.push(0, 0, -1);
+        //polo sul
+        for(let i = 0; i < this.slices+1; i++)
+        {
+            this.vertices.push(0, 0, -this.radius)
+            this.normals.push(0, 0, -1);
+            //this.texCoords.push();
+        }
+
         //stack numero 1
         for(let i = 0; i < this.slices; i++)
         {
@@ -30,18 +36,20 @@ class MySphere extends CGFobject
             var z=Math.sin(delta-Math.PI/2);
             this.vertices.push(x*this.radius, y*this.radius, z*this.radius);
             this.normals.push(x,y,z);             
-            this.texCoords.push( (Math.cos(i * angle)), Math.sin(delta-Math.PI/2));					
+            //this.texCoords.push();				
 
-            this.indices.push(0, (i+1)+1, i+1);
+            this.indices.push(i, this.slices+1+i+1, this.slices+1+i);
         }
 
+        //ponto extra para a stack 1
         var x=Math.cos(delta-Math.PI/2);
         var y=0;
         var z=Math.sin(delta-Math.PI/2);
         this.vertices.push(x*this.radius, y*this.radius, z*this.radius);
         this.normals.push(x,y,z);             
-        this.texCoords.push(1, 0);	
+        //this.texCoords.push();	
 
+        //stacks intermédias
 		for(let j = 1; j < (2*this.stacks); j++)
 		{		
             for(let i = 0; i < this.slices; i++)
@@ -53,10 +61,11 @@ class MySphere extends CGFobject
                 this.normals.push(x,y,z);            
                 this.texCoords.push( (Math.cos(i * angle)), -(Math.sin(i * angle)));					
 
-                this.indices.push(1+(this.slices+1)*j+(i+1), 1+(this.slices+1)*j+i, 1+(this.slices+1)*(j-1)+i);
-                this.indices.push(1+(this.slices+1)*(j-1)+i, 1+(this.slices+1)*(j-1)+(i+1), 1+(this.slices+1)*j+(i+1));
+                this.indices.push(this.slices+1+(this.slices+1)*j+(i+1), this.slices+1+(this.slices+1)*j+i, this.slices+1+(this.slices+1)*(j-1)+i);
+                this.indices.push(this.slices+1+(this.slices+1)*(j-1)+i, this.slices+1+(this.slices+1)*(j-1)+(i+1), this.slices+1+(this.slices+1)*j+(i+1));
             }
 
+            //vértice extra para cada stack
             var x=Math.cos(j*delta - Math.PI/2);
             var y=0;
             var z=Math.sin(j*delta - Math.PI/2);
@@ -66,10 +75,14 @@ class MySphere extends CGFobject
         }
         
         //ultima stack
-        this.vertices.push(0,0, this.radius);
-        this.normals.push(0,0, 1);
         for(let i = 0; i < this.slices; i++)
-            this.indices.push(1+(this.slices+1)*(2*this.stacks-1)+i+1, 1+(this.slices+1)*(2*this.stacks), 1+(this.slices+1)*(2*this.stacks-1)+i);
+        {   
+            this.vertices.push(0,0, this.radius);
+            this.normals.push(0,0, 1);
+            //this.texCoords.push();
+
+            this.indices.push((this.slices+1)+(this.slices+1)*(2*this.stacks-1)+i+1, (this.slices+1)+(this.slices+1)*(2*this.stacks)+i, (this.slices+1)+(this.slices+1)*(2*this.stacks-1)+i);
+        } 
         
 		this.primitiveType=this.scene.gl.TRIANGLES;
 

4e62a76e80d2d6013e77bf30de362b36f6156f58
leonormsousa <20341608+leonormsousa@users.noreply.github.com>
2019-10-01 14:48:29 +0100

    Finished normals for cylinder

--- Proj/MyCylinder.js
+++ Proj/MyCylinder.js
@@ -20,27 +20,29 @@ class MyCylinder extends CGFobject {
 		var delta = 2*Math.PI/this.slices;
 		var delta_y = this.height/this.stacks;
 
+		var norma = Math.sqrt(Math.pow(this.height) + Math.pow(this.base-this.top));
+
 		//vértices da face de baixo
 		for (i=0; i<=this.slices; i++)
 		{
-			this.vertices.push(Math.cos(delta*i)*this.base,0,(-Math.sin(delta*i))*this.base);
-			this.normals.push(Math.cos(delta*i),0,(-Math.sin(delta*i)));
+			this.vertices.push(Math.cos(delta*i)*this.base,(-Math.sin(delta*i))*this.base, 0);
+			this.normals.push(this.height*Math.cos(delta*i), -Math.sin(delta*i)*this.height, this.base-this.top);
 			this.texCoords.push(i/(this.slices), 0);
 		}
 
 		for (j=0; j<this.stacks; j++)
 		{
-			var radiusTop=  this.top*((j+1)/this.stacks) + this.base*(this.stacks-(j+1))/this.stacks;
+			var radiusTop = this.top*((j+1)/this.stacks) + this.base*(this.stacks-(j+1))/this.stacks;
 
 			for (i=0; i<=this.slices; i++)
 			{
 				//vértices da face de cima
-				this.vertices.push(Math.cos(delta*i)*radiusTop,(j+1)*delta_y,-Math.sin(delta*i)*radiusTop);
-				this.normals.push(Math.cos(delta*i),0,-Math.sin(delta*i));
+				this.vertices.push(Math.cos(delta*i)*radiusTop, -Math.sin(delta*i)*radiusTop, (j+1)*delta_y);
+				this.normals.push(this.height*Math.cos(delta*i), -Math.sin(delta*i)*this.height, this.base-this.top);
 				this.texCoords.push(i/(this.slices), j/this.stacks);
 				//definição das faces
-				this.indices.push(j*(this.slices+1)+i,j*(this.slices+1)+i+1,(j+1)*(this.slices+1)+i);
-				this.indices.push((j+1)*(this.slices+1)+i+1,(j+1)*(this.slices+1)+i, j*(this.slices+1)+i+1);
+				this.indices.push(j*(this.slices+1)+i ,(j+1)*(this.slices+1)+i, j*(this.slices+1)+i+1);
+				this.indices.push((j+1)*(this.slices+1)+i+1 , j*(this.slices+1)+i+1, (j+1)*(this.slices+1)+i);
 			}
 		}
 		

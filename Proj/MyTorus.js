3a142a5a56b75a3b02f33e44081bd333b548956a
SilviaRocha <silviajmr99@gmail.com>
2019-10-04 14:52:47 +0100

    Torus and Homer ready, materials and others missing

--- Proj/MyTorus.js
+++ Proj/MyTorus.js
@@ -21,27 +21,28 @@ class MyTorus extends CGFobject
 		var angle = (2 * Math.PI) / this.slices;
 		var delta = (2 * Math.PI) / this.loops;
 
-		for(let j = 0; j < this.loops; j++)
-		{		
-            for(let i = 0; i < this.slices; i++)
-            {
-                this.vertices.push((this.outer + this.inner*Math.cos(angle))*Math.cos(delta), (this.outer + this.inner*Math.cos(angle))*Math.sin(delta), this.inner*Math.sin(angle));
-                this.normals.push(Math.cos(angle)*Math.cos(delta), Math.cos(angle)*Math.sin(delta), Math.sin(angle));            
-                this.texCoords.push(i/this.slices, j/this.loops);					
-
-                this.indices.push((this.slices+1)*j+i, (this.slices+1)*j+ i+1, (this.slices+1)*(j+1)+ i+1);
-                this.indices.push((this.slices+1)*j+i, (this.slices+1)*(j+1)+ i, (this.slices+1)*(j+1)+ i+1);
-            }	
-            //vertices extra para cada slice
-            this.vertices.push((this.outer + this.inner)*Math.cos(delta), (this.outer + this.inner)*Math.sin(delta), 0);
-            this.normals.push(Math.cos(delta), Math.sin(delta), 0);            
-            this.texCoords.push(1, j/this.loops);
+        for(let i=0; i<this.slices+1; i++){
+            this.vertices.push(this.outer + this.inner*Math.cos(i*angle), 0, this.inner*Math.sin(i*angle));
+            this.normals.push(Math.cos(i*angle),0, Math.sin(i*angle));            
+            this.texCoords.push(i/this.slices,0);			
         }
 
-        //vertices extra para a loop final
-        this.vertices.push(this.outer + this.inner, 0, 0);
-        this.normals.push(1, 0, 0);            
-        this.texCoords.push(1, 1);
+		for(let j = 1; j <= this.loops+1; j++)
+		{		
+            for(let i = 0; i <= this.slices; i++)
+            {
+                this.vertices.push((this.outer + this.inner*Math.cos(i*angle))*Math.cos(j*delta), (this.outer + this.inner*Math.cos(i*angle))*Math.sin(j*delta), this.inner*Math.sin(i*angle));
+                this.normals.push(Math.cos(i*angle)*Math.cos(j*delta), Math.cos(i*angle)*Math.sin(j*delta), Math.sin(i*angle));            
+                this.texCoords.push(i/this.slices, j/this.loops);					
+
+                this.indices.push((this.slices+1)*(j-1)+i, (this.slices+1)*j+ i+1, (this.slices+1)*(j-1)+ i+1,);
+                this.indices.push((this.slices+1)*(j-1)+i,(this.slices+1)*j+ i, (this.slices+1)*j+ i+1);
+            }	
+            //vertices extra para cada slice
+            this.vertices.push((this.outer + this.inner)*Math.cos(j*delta), (this.outer + this.inner)*Math.sin(j*delta), 0);
+            this.normals.push(Math.cos(j*delta), Math.sin(j*delta), 0);            
+            this.texCoords.push(1, j/this.loops);
+        }
         
 		this.primitiveType=this.scene.gl.TRIANGLES;
 
